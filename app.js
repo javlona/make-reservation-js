33de4baabe66dded3d46462b48c50849881d23ba
javlona <joeclaap@gmail.com>
2021-11-21 14:32:46 -0600

    object functionality

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ let phone = document.querySelector('#phone');
 let mkReserveBtn = document.querySelector('#mkReserveBtn');
 let reservationTable = document.querySelector('#reservationTable'); //table tag
 let nameList = document.querySelector('#nameList');  // tbody tag
-let deleteIcon = '<i class="fas fa-trash text-center"></i>'
+let deleteIcon = '<i class="fas fa-trash text-center" onclick="deletePerson(event)"></i>'
 let editIcon = '<i class="fas fa-edit text-center"></i>'
+
+function Person(first, last, phone){
+    this.firstName = first;
+    this.LastName = last;
+    this.phone = phone;
+}
 
 let addToTable = () => {
     
@@ -22,27 +28,33 @@ let addToTable = () => {
 }
 
 let addPerson = () => {
-
+    //create tr tag
     let row = document.createElement('tr');
 
+    //add input values inside td into tr
     row.innerHTML = `
         <td>${editIcon}</td>
         <td>${firstName.value}</td>
         <td>${lastName.value}</td>
         <td>${phone.value}</td>
         <td>${deleteIcon}</td>
-
     `
-    
+    //push the ready tr row into tbody
     nameList.appendChild(row);
+
+    const person1 = new Person(firstName.value, lastName.value, phone.value);
+    console.log(person1);
 }
 
+//clear input fields after addition
 let inputClear = () => {
     firstName.value = "";
     lastName.value = "";
     phone.value = "";
 }
 
+//delete person on delete button
+let deletePerson = (e) => e.target.parentElement.parentElement.remove()
 
 
-mkReserveBtn.addEventListener("click", addToTable)
+mkReserveBtn.addEventListener("click", addToTable);
